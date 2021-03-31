8cdcf22e0a48eba452ba0fca0c5c115ea1b684d0
megaoy <edsocafe@yahoo.com.br>
2021-03-31 19:58:54 -0300

    clipa cobraa na tela artraves de wrappingtecnike

--- js/main.js
+++ js/main.js
@@ -9,6 +9,16 @@ snake[0]= {
 
 let direction = "right";
 
+document.addEventListener('keydown', update);
+
+function update (event) {
+    if(event.keyCode == 37 && direction != "right") {direction = "left";}
+    if(event.keyCode == 38 && direction != "down"){ direction = "up";}
+    if(event.keyCode == 39 && direction != "left"){ direction = "right";}
+    if(event.keyCode == 40 && direction != "up"){ direction = "down";}
+}
+
+
 function cria_bg(){
     context.fillStyle = "#74b9ff";
     context.fillRect(0,0,16 * box,16 * box);
@@ -22,6 +32,14 @@ function cria_cobra(){
 }
 
 function iniciar_jogo(){
+
+    if(snake[0].x > 15 * box && direction == "right"){ snake[0].x = 0;}
+    if(snake[0].x < 0 && direction == "left"){ snake[0].x = 16 * box;}
+    if(snake[0].y > 15 * box && direction == "down"){ snake[0].y = 0;}
+    if(snake[0].y < 0 && direction == "up"){ snake[0].y = 16 * box;}
+
+
+
     cria_bg();
     cria_cobra();
 
